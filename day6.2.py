17a71806d192aead5367168ab7be1bc22bb30406
Konstantinos Kyriakos Sitistas <81265818+sitistas@users.noreply.github.com>
2022-12-06 16:09:04 +0200

    Add day6

---
+++ day6.2.py
@@ -0,0 +1,22 @@
+# read file input6.txt
+f = open("input6.txt", "r")
+text = f.read()
+text = [*text]
+# print(text)
+
+last4 = []
+counter = 0
+for i in range(len(text)):
+    if text[i] in last4:
+        # find position of text[i] in last4
+        pos = last4.index(text[i])
+        for j in range(pos + 1):
+            del last4[0]
+        last4.append(text[i])
+    else:
+        last4.append(text[i])
+    counter += 1
+    if len(last4) == 14:
+        print(last4)
+        print(counter)
+        break
